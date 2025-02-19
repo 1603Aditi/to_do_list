b19bf6dc2cbc690b6a89fa3700515acce8164a89
1603Aditi <adititeena3458@gmail.com>
2025-02-19 13:55:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+    let resource = document.querySelector("#resources");
+    let dropdown = document.querySelector("#dropdownmenu");
+
+    resource.addEventListener("click", function (event) {
+        event.preventDefault(); 
+
+        if (dropdown.style.display === "none" || dropdown.style.display === "") {
+            dropdown.style.display = "block";
+        } 
+    });
+
+    resource.addEventListener("mouseleave", function (event) {
+        event.preventDefault(); 
+
+        if (dropdown.style.display === "block") {
+            dropdown.style.display = "none";
+        }
+    });
